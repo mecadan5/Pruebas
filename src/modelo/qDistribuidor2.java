1475c0354207bf570e405ab85c816723df3b406b
mecadan5 <mecadan5@gmail.com>
2012-12-12 12:52:11 -0500

    arreglos en edgar

--- src/modelo/qDistribuidor2.java
+++ src/modelo/qDistribuidor2.java
@@ -1,10 +1,15 @@
 package modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 
 @Entity
 public class qDistribuidor2 extends qUsuario2{
-	private qCliente2 listaDeClientes[];
+	
+	@OneToMany(mappedBy="listaDeCliente")
+	@JoinColumn(name="id_administrador", unique= true, nullable=true, insertable=true, updatable=true)
+	private List<qClientes2> listaDeCliente;
+	
 	private int tipoDeCuenta;
 	
 	public qDistribuidor2() {
